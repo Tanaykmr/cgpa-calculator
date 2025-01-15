957ce3419958a954898b8e17c97c8cc3727afe5b
Tanay Kumar <tanaykmrdev@gmail.com>
2025-01-15 05:37:15 +0530

    feat: added info section

--- src/appComponents/cgpaCalc.tsx
+++ src/appComponents/cgpaCalc.tsx
@@ -2,90 +2,8 @@
 import { useState, FormEvent } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import { ConfettiButton } from '@/components/ui/confetti';
-
-interface subjectInterface {
-	subjectName: string;
-	subjectCode: string;
-	paperId: string;
-	credits: number;
-	// marks: number;  TODO: do we need this?
-	// TODO: add lab type here to render theory and labs separately
-}
-
-const subjectsData: subjectInterface[] = [
-	{
-		subjectName: 'Economics for Engineers',
-		subjectCode: 'HS-301',
-		paperId: '031301',
-		credits: 2,
-	},
-	{
-		subjectName: 'Compiler Design',
-		subjectCode: 'CIC-303',
-		paperId: '031303',
-		credits: 3,
-	},
-	{
-		subjectName: 'Operating Systems',
-		subjectCode: 'CIC-305',
-		paperId: '031305',
-		credits: 4,
-	},
-	{
-		subjectName: 'Computer Networks',
-		subjectCode: 'CIC-307',
-		paperId: '031307',
-		credits: 4,
-	},
-	{
-		subjectName: 'Software Engineering',
-		subjectCode: 'CIC-309',
-		paperId: '031309',
-		credits: 3,
-	},
-	{
-		subjectName: 'Design and Analysis of Algorithm',
-		subjectCode: 'CIC-311',
-		paperId: '031311',
-		credits: 4,
-	},
-	{
-		subjectName: 'Compiler Design Lab',
-		subjectCode: 'CIC-351',
-		paperId: '031351',
-		credits: 2,
-	},
-	{
-		subjectName: 'Operating Systems Lab',
-		subjectCode: 'CIC-353',
-		paperId: '031353',
-		credits: 2,
-	},
-	{
-		subjectName: 'Computer Networks Lab',
-		subjectCode: 'CIC-355',
-		paperId: '031355',
-		credits: 2,
-	},
-	{
-		subjectName: 'Software Engineering Lab',
-		subjectCode: 'CIC-357',
-		paperId: '031357',
-		credits: 2,
-	},
-	{
-		subjectName: 'Design and Analysis of Algorithm Lab',
-		subjectCode: 'CIC-359',
-		paperId: '031359',
-		credits: 2,
-	},
-	{
-		subjectName: 'Summer Training Report - 1',
-		subjectCode: 'ES-361',
-		paperId: '031361',
-		credits: 2,
-	},
-];
+import { subjectsData } from '@/subjectData/subjectData';
+import eqn from '../../public/eqn.svg';
 
 // Generate initial data
 const initialData = subjectsData.map((subject) => ({
@@ -98,7 +16,16 @@ const initialData = subjectsData.map((subject) => ({
 	marks: 0,
 }));
 
-console.log('The initial data is: ' + JSON.stringify(initialData));
+const marksData = [
+	{ marksRange: '90-100', grade: 'O', gradePoint: 10 },
+	{ marksRange: '75-89', grade: 'A+', gradePoint: 9 },
+	{ marksRange: '65-74', grade: 'A', gradePoint: 8 },
+	{ marksRange: '55-64', grade: 'B+', gradePoint: 7 },
+	{ marksRange: '50-54', grade: 'B', gradePoint: 6 },
+	{ marksRange: '45-49', grade: 'C', gradePoint: 5 },
+	{ marksRange: '40-44', grade: 'P', gradePoint: 4 },
+	{ marksRange: 'Less than 40 or absent', grade: 'F', gradePoint: 0 },
+];
 
 export default function CGPACalculator() {
 	const [data, setData] = useState(initialData);
@@ -106,9 +33,6 @@ export default function CGPACalculator() {
 
 	const handleInputChange = (index: number, field: string, value: number): void => {
 		const newData = [...data];
-		// if (field === "credits" || field === "marks") {
-		// 	value = Number(value); // Converts string to number
-		// }
 		newData[index] = { ...newData[index], [field]: value };
 		setData(newData);
 	};
@@ -140,14 +64,8 @@ export default function CGPACalculator() {
 
 		data.forEach((row) => {
 			const credits = row.credits;
-			console.log('typeof credits: ' + typeof credits);
-			console.log('typeof credit2: ' + typeof row.credits);
 			const marks = row.marks;
-			console.log('typeof marks: ' + typeof marks);
-			// if (!isNaN(credits) && !isNaN(marks)) {
-			console.log('adding ' + credits + ' credits');
 			totalCredits += credits;
-			console.log('The tc are: ' + totalCredits);
 			// Simple grade point calculation (adjust as needed)
 			const gradePoint =
 				marks >= 90
@@ -164,27 +82,18 @@ export default function CGPACalculator() {
 										? 5
 										: 0;
 			totalGradePoints += credits * gradePoint;
-			console.log('The credits are: ' + credits);
-			console.log('The total credits are: ' + totalCredits);
-			console.log('The marks are: ' + marks);
-			console.log('The total grade points are: ' + totalGradePoints);
-			// }
 		});
-		console.log('--------------------------');
-		console.log('The total grade points are: ' + totalGradePoints);
-		console.log('The total credits are: ' + totalCredits);
 		const calculatedCGPA = totalGradePoints / totalCredits;
-		console.log('The calculated CGPA is: ' + calculatedCGPA);
 		setCGPA(isNaN(calculatedCGPA) ? null : parseFloat(calculatedCGPA.toFixed(3)));
 
 		scrollToBottom();
 	};
 
 	return (
-		<div className="container mx-auto rounded-lg border bg-white p-4 shadow-md">
-			<h1 className="mb-4 text-2xl font-bold">CGPA Calculator*</h1>
-			<div className="mb-4">* Only valid for 5th sem IT students</div>
-			<form onSubmit={calculateCGPA}>
+		<div className="container mx-auto flex flex-col gap-2 rounded-lg border bg-white p-4 shadow-md">
+			<h1 className="text-2xl font-bold text-[#041E54]">CGPA Calculator*</h1>
+			<div className="mb-2">* Only valid for 5th sem CSE and IT students</div>
+			<form onSubmit={calculateCGPA} className="mb-2">
 				<div className="overflow-x-auto">
 					<table className="w-full border-collapse border border-gray-300">
 						<thead>
@@ -209,13 +118,9 @@ export default function CGPACalculator() {
 											required
 											type="number"
 											value={row.credits}
-											onChange={(e) => {
-												handleInputChange(index, 'credits', parseInt(e.target.value));
-												console.log(
-													'The typeof credits in handleInputChange are: ' +
-														typeof row.credits,
-												);
-											}}
+											onChange={(e) =>
+												handleInputChange(index, 'credits', parseInt(e.target.value))
+											}
 											min="1"
 											max="5"
 											className="hover: w-full rounded-md border-gray-400 p-1 hover:border-2" // todo: maybe add touch interactions here and in marks??
@@ -227,12 +132,9 @@ export default function CGPACalculator() {
 											required
 											type="number"
 											value={row.marks}
-											onChange={(e) => {
-												handleInputChange(index, 'marks', parseInt(e.target.value));
-												console.log(
-													'The typeof marks in handleInputChange are: ' + typeof row.marks,
-												);
-											}}
+											onChange={(e) =>
+												handleInputChange(index, 'marks', parseInt(e.target.value))
+											}
 											min="0"
 											max="100"
 											className="w-full rounded-md border-2 border-gray-400 p-1"
@@ -248,7 +150,6 @@ export default function CGPACalculator() {
 					<button
 						type="button"
 						onClick={addNewRow}
-						// className="flex items-center rounded-lg border border-gray-300 p-2 transition-transform active:translate-y-0.5 active:shadow"
 						className="flex items-center rounded-lg border border-gray-300 bg-white p-3 text-gray-800 transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 hover:shadow active:translate-y-0.5 active:bg-gray-200"
 					>
 						<AddIcon className="mr-2 h-4 w-4" />
@@ -274,6 +175,121 @@ export default function CGPACalculator() {
 					<p className="text-xl font-bold">Your CGPA: {cgpa}</p>
 				</div>
 			)}
+			<div className="info-div mt-4 flex flex-col gap-4">
+				<div>
+					<h2 className="text-2xl font-bold text-[#0955A0]">How to calculate CGPA for IPUniversity?</h2>
+					<div>The formula for calculation of CGPA is given below:</div>
+					<div>
+						<img src={eqn} alt="CGPA Formula" className="rounded-sm bg-gray-300 p-2" />
+						Where <br /> C<sub>ni</sub> - number of credits of the ith course of the nth semester. <br />G
+						<sub>ni</sub> - grade points of the ith course of the nth semester
+					</div>
+				</div>
+
+				<div>
+					<h2 className="text-2xl font-bold text-[#0955A0]">In easier words:</h2>
+					<div>
+						<ol className="ml-4 list-decimal">
+							<li>We calculate the product of 'credits and grade point' for each subject and add them</li>
+							<li>We then divide the result obtained above by the total number of credits</li>
+							<li>Lastly, the final result is rounded off to 2 decimal places</li>
+						</ol>
+					</div>
+				</div>
+				<div>
+					<h2 className="text-2xl font-bold text-[#0955A0]">Grading System</h2>
+					<p>
+						The marks that a student secures from the maximum 100 are to be converted into a grade as a
+						letter. <br />
+						The grade points are the numerical equivalent of the letter grade assigned to a student based on
+						the total marks obtained by the student. <br />
+						These grade points and letter grades are based on the points scale as given below:
+					</p>
+					<div className="my-2 flex justify-center">
+						<table className="mx-auto w-3/5 border-collapse border-2 shadow-lg">
+							<thead>
+								<tr className="bg-gray-100">
+									<th className="border-2 border-gray-700 p-2 text-center">Marks</th>
+									<th className="border-2 border-gray-700 p-2 text-center">Grade</th>
+									<th className="border-2 border-gray-700 p-2 text-center">Grade Point</th>
+								</tr>
+							</thead>
+							<tbody>
+								{marksData.map((row, index) => (
+									<tr key={index}>
+										<td className="border-2 border-gray-700 p-2 text-center">{row.marksRange}</td>
+										<td className="border-2 border-gray-700 p-2 text-center">{row.grade}</td>
+										<td className="border-2 border-gray-700 p-2 text-center">{row.gradePoint}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
+					<p>
+						<ul className="ml-3 list-disc">
+							<li>
+								Grade P, that is the grade point 4 is the course passing grade unless specified
+								otherwise by the Syllabi and Scheme of Teaching and Examination for the respective
+								programme.
+							</li>
+						</ul>
+						<li>
+							For grades below the passing grade (P) as defined in the Syllabi and Scheme of Teaching and
+							Examination, the associated grade points are to be taken equal to zero.
+						</li>
+						<li>Both the acquired marks and grades are to be reflected on the term end marksheets.</li>
+					</p>
+				</div>
+				<div>
+					<h2 className="text-2xl font-bold text-[#0955A0]">CGPA Divisions:</h2>
+					<p>
+						The successful candidates having an overall CGPA higher than or equal to the minimum CGPA that
+						is specified in the Syllabi and Scheme of Teaching and Examination for the award of the degree,
+						are to be awarded the degree and be placed in Divisions as below:
+						<ul className="ml-3 list-disc">
+							<li>CGPA of 6.50 or above are to be placed in the First Division.</li>
+							<li>CGPA of 5.00 - 6.49 are to be placed in the Second Division.</li>
+							<li>CGPA of 4.00 - 4.99 are to be placed in the Third Division.</li>
+							<li>
+								CGPA of 10 are to be placed in the Exemplary Performance. Exemplary Performance shall be
+								awarded, if and only if, every course of the programme offered to the student is passed
+								in the first chance of appearing in the paper that is offered to the student. A student
+								with an academic break shall not be awarded the exemplary performance.
+							</li>
+							<li>
+								The CGPA x 10 shall be deemed equivalent to percentage of marks obtained by the student
+								for the purpose of equivalence to percentage of marks.
+							</li>
+						</ul>
+					</p>
+				</div>
+				<p className="italic">
+					Note: <br />
+					This IPU CGPA Calculator works on the algorithm provided by IPUniversity in the ordinance 11. <br />
+					<ul className="ml-3 list-disc">
+						<li>
+							<a
+								href="http://ipu.ac.in/norms/Ordinance/oridancemain.htm"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-blue-600 underline visited:text-purple-600"
+							>
+								University School of Education, GGSIPU (University Ordinance)
+							</a>
+						</li>
+						<li>
+							<a
+								href="http://ipu.ac.in/norms/Ordinance/ordinanc11020815.pdf"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-blue-600 underline visited:text-purple-600"
+							>
+								Ordinance 11
+							</a>
+						</li>
+					</ul>
+				</p>
+			</div>
 		</div>
 	);
 }
